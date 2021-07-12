c1f4ba90a46144da818bd52498cb24346eeb68e7
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-07-12 23:14:37 +0530

    addded login and editdata components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css';
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Home from './components/Home'
 import Main from "./components/Main";
 import CreateData from './components/CreateData';
 import EditData from './components/EditData';
+import './App.css';
+import Login from "./components/Login";
 
 function App() {
 
   return (
     <Router>
       <div className="App">
-        <h2>Bank</h2>
         <Switch>
-          <Route path="/" component = {Main} exact />
-          <Route exact path="/createdata" component={CreateData} exact/>
+          <Route path="/" component = {Login} exact />
+          <Route path="/home" component = {Home} exact />
+          <Route path="/ifsc" component = {Main} exact />
+          <Route path="/createdata" component={CreateData} exact/>
           <Route  path="/editdata/:ifsccode" component={EditData} exact/>
         </Switch>
       </div>
